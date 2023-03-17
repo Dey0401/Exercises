a43e7506b7c96afcbf1269d275b47f8ad8b0443f
Deyssi Vergara <deyssiyuliana@gmail.com>
2023-03-17 17:13:55 -0500

    menu creation, the beam object is constructed.

--- Exercises.Logic/Exercises.UI/Program.cs
+++ Exercises.Logic/Exercises.UI/Program.cs
@@ -1,2 +1,53 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using Exercises.Logic;
+
+try
+{
+    int option;
+    do
+    {
+        option = Menu();
+        switch (option)
+        {
+            case 1:
+                Console.WriteLine("Ingrese la estructura de la viga: ");
+                var stringBeam = new Beam(Console.ReadLine());
+
+                break;
+            case 2:
+                Console.WriteLine("Ingrese la estructura del puente: ");
+                var stringBridge = Console.ReadLine();
+                break;
+            default:
+                Console.WriteLine("Fuck you. Opción no existe!!!!");
+                break;
+        }
+    } while (option != 0);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+static int Menu()
+{
+    Console.WriteLine("¿Que desea construir?");
+    Console.WriteLine("1. Una viga");
+    Console.WriteLine("2. Un puente");
+    Console.WriteLine("0. Salir");
+    bool isValid = false;
+    int option = 0;
+    do
+    {
+        Console.Write("Digite su opción: ");
+        var optionString = Console.ReadLine();
+        if (!int.TryParse(optionString, out option))
+        {
+            Console.WriteLine("Opción inválida, solo use números.");
+            isValid = false;
+        }
+        else
+        {
+            isValid = true;
+        }
+    } while (!isValid);
+    return option;
+}
